a47bcc11be5c739af5b6e517bf3ee83e16812200
TKD498 <driggskingtate@gmail.com>
2019-07-08 15:14:19 +0530

    Number Even or Odd Algorithm

---
+++ PRE-BC-A-EvenOrOdd.js
@@ -0,0 +1,9 @@
+//-------------------Even-or-Odd-------------------
+ var input = prompt("Pick a number any Number");
+
+     	if (input % 2 == 0){
+              	alert("Thats an Even number");
+              }
+
+              else {
+              	alert("Thats an odd number") */
