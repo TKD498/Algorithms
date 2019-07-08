a6dbff101844969fc5cf3804ed0736c36c09183e
TKD498 <driggskingtate@gmail.com>
2019-07-08 15:12:30 +0530

    Inputed Number Even or Odd Algorithm

---
+++ Positive-Negative-Number-Checker.js
@@ -0,0 +1,10 @@
+//-------------------+or-checker-------------------
+ var userNum = prompt("Pick a number")
+
+   if (userNum > 0){
+     alert("Thats a positive Number")
+ } if (userNum < 0) {
+     alert("Thats a negative Number")
+ } if (userNum == 0) {
+     alert("0 is a neurtal number")
+ }
