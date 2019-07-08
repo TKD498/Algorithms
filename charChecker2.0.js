4a1500db449d9b8a221e48c8cb0c32c3c6fa556b
TKD498 <driggskingtate@gmail.com>
2019-07-08 11:02:58 +0530

    character checking algorithm using ASCII codes

---
+++ charChecker2.0.js
@@ -0,0 +1,7 @@
+var char = prompt("Pick a character and i will tell you if its apart of the alphabet")
+
+if((char>='a' && char<='z') || (char>='A' && c<='Z')){
+	console.log("is an alphabet.");
+} else {
+	console.log("is not an alphabet.");
+}
